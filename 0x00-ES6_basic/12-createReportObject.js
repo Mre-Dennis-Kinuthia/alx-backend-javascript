36faa3f274b42ae07fb739e63121cae361f47bab
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-08-28 11:12:10 +0300

    12 added

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,8 @@
-expo
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
+}
